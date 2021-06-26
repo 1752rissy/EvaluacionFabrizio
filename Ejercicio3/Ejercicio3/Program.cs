95778c195966304082d1f25dd8f539c8ddcc5f45
martin deramo rojo <fabry_rojin_2013@hotmail.com>
2021-06-26 12:53:26 -0300

    Cuarto commit

--- Ejercicio3/Ejercicio3/Program.cs
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -14,7 +14,20 @@ namespace Ejercicio3
             Materia materia = new Materia();
             Alumno alumno = new Alumno(materia);
             Profesor profesor = new Profesor();
-           
+            OperacionesABM operacionesabm = new OperacionesABM(alumno);
+            OperacionesABM operacionesabm2 = new OperacionesABM(profesor);
+            OperacionesABM operacionesabm3 = new OperacionesABM(materia);
+            OperacionesABM operacionesabm4 = new OperacionesABM(carrera);
+            OperacionesBusqueda operacionbusq = new OperacionesBusqueda(alumno);
+            OperacionesBusqueda operacionbusq2 = new OperacionesBusqueda(profesor);
+            Console.WriteLine(operacionbusq.BuscarXDni());
+            Console.WriteLine(operacionbusq2.BuscarXDni());
+            Console.WriteLine(operacionesabm.Alta());
+            Console.WriteLine(operacionesabm2.Baja());
+            Console.WriteLine(operacionesabm3.Modificar());
+            Console.WriteLine(operacionesabm4.Alta());
+            Console.ReadLine();
+            
 
 
         }
